c472c54130d9ea65b2fa3eeaf350580f65ef51ef
moamen2020 <m01144944981@gmail.com>
2021-01-28 07:52:29 +0200

    ubdate menu-item to navigate WithRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage-component";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact to="/" component={HomePage} />
+      </Switch>
     </div>
   );
 }
